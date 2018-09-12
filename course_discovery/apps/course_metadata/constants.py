0eb9beb3d8e14d2bb753b0610461ad66fbcff66b
wowkalucky <wowkalucky@gmail.com>
2018-09-12 18:54:57 +0300

    Define bundles generation rules

--- course_discovery/apps/course_metadata/constants.py
+++ course_discovery/apps/course_metadata/constants.py
@@ -1,2 +1,16 @@
 COURSE_ID_REGEX = r'[^/+]+(/|\+)[^/+]+'
 COURSE_RUN_ID_REGEX = r'[^/+]+(/|\+)[^/+]+(/|\+)[^/]+'
+
+# Rules for automated Programs creation:
+RULES_PROGRAM_TYPE_NAME = 'Rules'
+PROGRAM_RULES = [
+    {'name': '7/2-SPEC', 7: 2, 'spec': False},
+    {'name': '7/2+SPEC', 7: 2, 'spec': True},
+    {'name': '7/3', 7: 3},
+    {'name': '8/1|7/1|3/2', 8: 1, 7: 1, 3: 2},
+    {'name': '7/2|3/2-SPEC', 7: 2, 3: 2, 'spec': False},
+    {'name': '7/4', 7: 4},
+    {'name': '8/1|7/2|3/2', 8: 1, 7: 2, 3: 2},
+    {'name': '7/3|3/3', 7: 3, 3: 3},
+    {'name': '8/1|7/5|4/1|3/1', 8: 1, 7: 5, 4: 1, 3: 1},
+]
