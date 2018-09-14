0e4337658e1015a0465ca1444a6225a26659a0c1
wowkalucky <wowkalucky@gmail.com>
2018-09-14 10:35:58 +0300

    Actualize bundles generation rules

--- course_discovery/apps/course_metadata/constants.py
+++ course_discovery/apps/course_metadata/constants.py
@@ -4,11 +4,11 @@ COURSE_RUN_ID_REGEX = r'[^/+]+(/|\+)[^/+]+(/|\+)[^/]+'
 # Rules for automated Programs creation:
 RULES_PROGRAM_TYPE_NAME = 'Rules'
 PROGRAM_RULES = [
-    {'name': '7/2-SPEC', 7: 2, 'spec': False},
-    {'name': '7/2+SPEC', 7: 2, 'spec': True},
+    {'name': '7/2-USPAP', 7: 2, 'uspap': False},
+    {'name': '7/2+USPAP', 7: 2, 'uspap': True},
     {'name': '7/3', 7: 3},
     {'name': '8/1|7/1|3/2', 8: 1, 7: 1, 3: 2},
-    {'name': '7/2|3/2-SPEC', 7: 2, 3: 2, 'spec': False},
+    {'name': '7/2|3/2-USPAP', 7: 2, 3: 2, 'uspap': False},
     {'name': '7/4', 7: 4},
     {'name': '8/1|7/2|3/2', 8: 1, 7: 2, 3: 2},
     {'name': '7/3|3/3', 7: 3, 3: 3},
